39e873661a3d007e00a82d975cc056d777c968e2
kunkaffa <kunkaffa3@gmail.com>
2025-11-01 23:07:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -101,16 +101,17 @@ def main():
     print(f"\r\033[97m║\033[32m   https://kunkaffa@gmail.com{' ' * 42}\033[97m║")
     print(f"\r\033[97m╚{'═' * 71}╝")
     while attemps < 100:
-      username = input("\033[32mEnter your username: \033[0m")
-      password = input("\033[31mEnter your password: \033[0m")
+    print("\033[103m┏━━KunFayz━━⬣")
+    username = input("\033[103m┗> Enter your username: \033[103m ")
+    password = input("\033[32m┗> Enter your password: \033[0m")
 
-      if username == 'admin' and password == 'admin':
-          print("\033[32m⟩⟩ Hai...! Welcome to zona attack BLACKPHANTER \033[0m")
-          break
-      else:
-          print('Incorrect credentials. Check if you have Caps lock on and try again.')
-          attemps += 1
-          continue
+    if username == 'admin' and password == 'admin':
+        print("\033[100m \033[31m••> ZONA FIGHT ZI0NIST \033[0m")
+        break
+    else:
+        print('Incorrect credentials. Check if you have Caps lock on and try again.')
+        attemps += 1
+        continue
     print(Fore.CYAN + "┏━━KunFayz━━⬣")
     print(Fore.CYAN + "┗> " + Fore.YELLOW + "1. Layer 4 Attack")
     print(Fore.CYAN + "┗> " + Fore.YELLOW + "2. Layer 7 HTTP Attack")
